c319c5d059acc5565c29762c84c6eaaf2bc8ea44
srhrsb <sebastian.boettcher2@srh.de>
2024-07-12 11:15:46 +0200

    Initial commit

---
+++ src/main/java/com/brh/mp3player/Controller.java
@@ -0,0 +1,46 @@
+package com.brh.mp3player;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+public class Controller {
+
+    private final MusicPlayer musicPlayer = new MusicPlayer();
+
+    @FXML
+    private Label fileLabel;
+
+    @FXML
+    private Slider volumeSlider;
+
+
+    @FXML
+    protected void onFile(){
+
+        Stage stage = (Stage) fileLabel.getScene().getWindow();
+        FileChooser fileChooser = new FileChooser();
+        File file = fileChooser.showOpenDialog(stage);
+        fileLabel.setText(file.getAbsolutePath());
+    }
+
+    @FXML
+    protected void onPlay(){
+         String file = fileLabel.getText();
+         musicPlayer.play(file);
+    }
+
+    @FXML
+    protected void onStop(){
+        musicPlayer.stop();
+    }
+
+    @FXML
+    protected void onVolume(){
+        musicPlayer.setVolume( volumeSlider.getValue() );
+    }
+}
---
+++ src/main/java/com/brh/mp3player/MusicPlayer.java
@@ -0,0 +1,32 @@
+package com.brh.mp3player;
+
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+import java.io.File;
+
+public class MusicPlayer {
+
+    private MediaPlayer mp;
+
+    public void play( String path ){
+
+        Media media = new Media( new File(path).toURI().toString() );
+        mp = new MediaPlayer( media );
+        mp.stop();
+        mp.play();
+
+    }
+
+    public void stop(){
+        if(mp == null) return;
+        mp.stop();
+    }
+
+    public void setVolume( double vol ){
+        if(mp == null) return;
+        mp.setVolume( vol );
+    }
+
+
+}
